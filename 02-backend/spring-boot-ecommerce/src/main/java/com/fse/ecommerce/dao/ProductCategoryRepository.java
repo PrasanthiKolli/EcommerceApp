0093155e32238182e9ab3aa467436a964c7e7e2e
12 <2>
2024-03-31 11:41:06 +0530

    product list impl

--- 02-backend/spring-boot-ecommerce/src/main/java/com/fse/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/fse/ecommerce/dao/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.fse.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.fse.ecommerce.entity.ProductCategory;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "ProductCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- 02-backend/spring-boot-ecommerce/src/main/java/com/fse/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/fse/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.fse.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.fse.ecommerce.entity.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
