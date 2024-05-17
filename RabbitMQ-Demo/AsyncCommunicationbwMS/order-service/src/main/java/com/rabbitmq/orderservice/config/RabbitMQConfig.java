ebf4b66ba8b1bd0faec88e8dd78703f3ad4a4e5d
12 <12>
2024-05-17 11:51:24 +0530

    microservices impl

---
+++ RabbitMQ-Demo/AsyncCommunicationbwMS/order-service/src/main/java/com/rabbitmq/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,54 @@
+package com.rabbitmq.orderservice.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class RabbitMQConfig {
+
+	@Value("${rabbitmq.queue.order.name}")
+	private String queue;
+
+	@Value("${rabbitmq.exchange.order.name}")
+	private String exchange;
+
+	@Value("${rabbitmq.order.routing.key}")
+	private String routingKey;
+
+	@Bean
+	public Queue queue() {
+		return new Queue(queue);
+	}
+
+	@Bean
+	public TopicExchange exchange() {
+		return new TopicExchange(exchange);
+	}
+
+	@Bean
+	public Binding binding() {
+		return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+	}
+
+	@Bean
+	public MessageConverter jsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jsonMessageConverter());
+		return rabbitTemplate;
+
+	}
+
+}
